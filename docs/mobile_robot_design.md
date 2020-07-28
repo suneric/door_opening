73b2cc76a47b65acd5a55325b254089b877b64b2
suneric <syf_more@163.com>
2020-07-28 01:27:04 -0400

    door opening demo

--- docs/mobile_robot_design.md
+++ docs/mobile_robot_design.md
@@ -8,4 +8,12 @@
   - platform (length x width x height): 50 cm x 50 cm x 50 cm (6 kg)
   - bumper (length x width x height): 4 cm x 50 cm x 12.5 cm (1 kg x 2)
 
+## device design
+- base bar (length x width x height): 2 cm x 20 cm x 100 cm
+- vertical slider (length x width x height): 2 cm x 50 cm x 5 cm
+- horizontal slider (length x width x height): 2 cm x 5 cm x 5 cm
+- finger (length x width x height): 10 cm x 2 cm x 2 cm
+- finger tip (length x width x height): 1 cm x 2 cm x 6 cm
+the length from tip to center of mobile base (in x axis) is 42 cm
+
 ## controller
--- dor_control/scripts/tasks.py
+++ dor_control/scripts/tasks.py
@@ -15,15 +15,15 @@ class MoveTask:
         self.goal = goal
         self.goal_status = 'ready' # moving, reached
         self.task_status = 'ready' # inprogress, done
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.pose_sub = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.amcl_cb)
+        self.amcl_pose = goal.position
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.client.wait_for_server()
         self.last_time = rospy.Time.now()
-        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.pose_sub = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.amcl_cb)
-        self.amcl_pose = None
 
     def is_done(self):
-        if self.task_status == 'done':
+        if self.task_status == 'done' or self.task_status == 'ready':
             return True
         else:
             return False
@@ -125,6 +125,35 @@ class ChargeTask(MoveTask):
         if self.goal_status == 'reached':
             self.auto_charge()
 
+# disinfection task
+class DisinfectTask(MoveTask):
+    def __init__(self, pos):
+        MoveTask.__init__(self,pos)
+        self.disinfect_time = rospy.Time.now()
+
+    def disinfect(self):
+        if self.task_status == 'ready':
+            self.task_status = 'inprogress'
+            rospy.loginfo("disinfecting (30 seconds)...")
+            self.disinfect_time = rospy.Time.now()
+        elif self.task_status == 'inprogress':
+            current_time = rospy.Time.now()
+            duration = current_time.secs - self.disinfect_time.secs
+            if duration < 30:
+                self.drive_rotation(1.0)
+            else:
+                self.stop_drive()
+                self.task_status = 'done'
+                rospy.loginfo("disinfection done")
+        else:
+            rospy.loginfo("disinfection done")
+
+    def perform(self):
+        if self.goal_status == 'ready':
+            self.auto_move()
+        if self.goal_status == 'reached':
+            self.disinfect()
+
 # push door task
 class PushDoorTask(MoveTask):
     def __init__(self,pos):
@@ -169,62 +198,82 @@ class PushDoorTask(MoveTask):
 
 # pull door task
 class PullDoorTask(MoveTask):
-    def __init__(self, pos):
+    def __init__(self,pos):
         MoveTask.__init__(self,pos)
         self.vslider_pub = rospy.Publisher('mobile_robot/joint_vertical_controller/command', Float64, queue_size=1)
         self.hslider_pub = rospy.Publisher('mobile_robot/joint_horizontal_controller/command', Float64, queue_size=1)
         self.hook_pub = rospy.Publisher('mobile_robot/joint_hook_controller/command', Float64, queue_size=1)
+        self.task_status = 'inprogress'
+        self.goal_status = 'reached'
+
+    def open_door_1(self):
+        self.stop_drive()
+        rospy.sleep(2)
+        self.vslider_pub.publish(0.88)
+        rospy.sleep(2)
+        self.drive_forward(-0.5)
+        rospy.sleep(2)
+        self.hook_pub.publish(1.57)
+        rospy.sleep(2)
+        self.drive_rotation(-1.0)
+        rospy.sleep(2)
+        self.vslider_pub.publish(1.0)
+        self.hslider_pub.publish(-0.24)
+        self.stop_drive()
+    def open_door_2(self):
+        for i in range(0,3):
+            self.drive_rotation(-1.0)
+            rospy.sleep(2)
+            self.drive_forward(-1.0)
+            rospy.sleep(1)
+        for i in range(0,4):
+            self.drive_rotation(-1.0)
+            rospy.sleep(5)
+            self.drive_forward(-1.0)
+            rospy.sleep(1)
+        for i in range(0,2):
+            self.drive_rotation(-3.0)
+            rospy.sleep(5)
+            self.drive_forward(1.0)
+            rospy.sleep(1.5)
+        self.drive_rotation(-3.0)
+        rospy.sleep(17)
+        self.drive_forward(-2.0)
+        rospy.sleep(12)
+        self.hook_pub.publish(0.0)
+        self.hslider_pub.publish(0.0)
+        self.stop_drive()
+        self.task_status = 'done'
 
     def pull_door(self):
         if self.task_status == 'ready':
             self.task_status = 'inprogress'
             rospy.loginfo("pulling door...")
-            self.vslider_pub.publish(1.0)
-            self.hslider_pub.publish(0.0)
-            self.hook_pub.publish(0.0)
         elif self.task_status == 'inprogress':
             # apprach the door
+            self.vslider_pub.publish(1.0)
+            self.hslider_pub.publish(0.0)
+            # print(self.amcl_pose)
+            if self.amcl_pose.x < -0.45:
+                self.drive_forward(0.5)
+            # elif self.amcl_pose.x > 2.0:
+            #     self.stop_drive()
+            #     self.task_status = 'done'
+            #     rospy.loginfo("pull door done")
+            else:
+                self.open_door_1()
+                self.open_door_2()
+
             # grab the door handle
             # pull the door
             # release the hook
             # rotate the robot using hook to hold the door and open the door
-            self.task_status = 'done'
-            rospy.loginfo("pull door done")
 
     def perform(self):
         if self.goal_status == 'ready':
             self.auto_move()
         if self.goal_status == 'reached':
             self.pull_door()
-
-# disinfection task
-class DisinfectTask(MoveTask):
-    def __init__(self, pos):
-        MoveTask.__init__(self,pos)
-        self.disinfect_time = rospy.Time.now()
-
-    def disinfect(self):
-        if self.task_status == 'ready':
-            self.task_status = 'inprogress'
-            rospy.loginfo("disinfecting (30 seconds)...")
-            self.disinfect_time = rospy.Time.now()
-        elif self.task_status == 'inprogress':
-            current_time = rospy.Time.now()
-            duration = current_time.secs - self.disinfect_time.secs
-            if duration < 30:
-                self.drive_rotation(1.0)
-            else:
-                self.stop_drive()
-                self.task_status = 'done'
-                rospy.loginfo("disinfection done")
-        else:
-            rospy.loginfo("disinfection done")
-
-    def perform(self):
-        if self.goal_status == 'ready':
-            self.auto_move()
-        if self.goal_status == 'reached':
-            self.disinfect()
 
 # positions
 def task_pose(x,y,yaw):
@@ -245,22 +294,26 @@ if __name__ == '__main__':
     task1 = ChargeTask(task_pose(7.5,1.0,0.0))
     task2 = DisinfectTask(task_pose(5.0,5.0,1.57))
     task3 = PushDoorTask(task_pose(2.5,5.5,3.14))
-    task4 = PullDoorTask(task_pose(-1.0,0.6,0.0))
+    task4 = PullDoorTask(task_pose(-1.0,0.8,0.0))
     try:
         while not rospy.is_shutdown():
-            if not task1.is_done():
-                task1.perform()
+            if not task4.is_done():
+                task4.perform()
             else:
-                if not task2.is_done():
-                    task2.perform()
-                else:
-                    if not task3.is_done():
-                        task3.perform()
-                    else:
-                        if not task4.is_done():
-                            task4.perform()
-                        else:
-                            rospy.loginfo("all tasks are performed.")
+                rospy.loginfo("done")
+            # if not task1.is_done():
+            #     task1.perform()
+            # else:
+            #     if not task2.is_done():
+            #         task2.perform()
+            #     else:
+            #         if not task3.is_done():
+            #             task3.perform()
+            #         else:
+            #             if not task4.is_done():
+            #                 task4.perform()
+            #             else:
+            #                 rospy.loginfo("all tasks are performed.")
         rate.sleep()
     except rospy.ROSInterruptException:
         pass
